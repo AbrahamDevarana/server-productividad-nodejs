0489ba14366306962354d67f2f55fb6aaf6f6d17
AbrahamAG <abrahamalvarado@devarana.mx>
2022-05-31 13:32:58 -0500

    Correccion 3

--- routes/regularAuth.js
+++ routes/regularAuth.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const {check} = require('express-validator');
 
 const authController = require('../controller/AuthController');
+const userController = require('../controller/UserController');
 
 router.post('/',     
     [   // Validate fields
@@ -15,5 +16,18 @@ router.get('/verifyUser',
     authController.verifyUser
 );
 
+
+router.post('/register',
+    ///Validaciones
+    [
+        check('name').not().isEmpty().withMessage('El nombre es requerido').trim(),
+        check('email').isEmail().withMessage('El correo es requerido').normalizeEmail(),
+        check('lastName').not().isEmpty().withMessage('El apellido es requerido'),
+        check('birth_date').not().isEmpty().withMessage('La fecha de nacimiento es requerida'),
+        check('admission_date').not().isEmpty().withMessage('La fecha de ingreso es requerida'),
+        check('phone').not().isEmpty().withMessage('El telefono es requerido'),
+    ],
+    userController.createUser
+);
 
 module.exports = router;
