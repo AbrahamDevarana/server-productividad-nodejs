b2909da7cab4b5b2fed50bc57e6f0ecdd8b04c56
Abraham Alvarado <abrahamalvarado@devarana.mx>
2023-02-21 13:33:08 -0600

    Cors

--- index.js
+++ index.js
@@ -20,7 +20,13 @@ const COOKIE_SECRET = process.env.COOKIE_SECRET;
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-app.use(cors( { origin: '*', credentials: true } ));
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}))
+
+console.log(process.env.CLIENT_URL);
+
 
 app.use(cookieSession({
     secret: COOKIE_SECRET,
